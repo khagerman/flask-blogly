09a77ae939f64e4068de310db52db5c21383524e
khagerman <71734063+khagerman@users.noreply.github.com>
2021-05-02 13:06:43 -0500

    ceated templates

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config['SECRET_KEY'] = "dogsarecool21837"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def list_users():
+    """redirect to list of all users from homepage"""
+  return redirect("/users")
+
+@app.route('/users')
+def list_users():
+    """Shows list of all users in db"""
+   user = User.query.all()
+    return render_template('list.html', users=users)
+
+@app.route('/users/new', methods=["POST"])
+def create_pet():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    img = request.form["img"]
+  
+    new_user  = User(first_name=first_name, last_name=last_name, img=img)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(f'/{new_user.id}')
--- models.py
+++ models.py
@@ -1,1 +1,22 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+# MODELS GO BELOW!
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    first_name = db.Column(db.String(50), nullable=False, unique=False)
+    last_name = db.Column(db.String(50), nullable=False, unique=False)
+
+    image_url = db.Column(db.text, default="/images/user-circle-solid.svg")
